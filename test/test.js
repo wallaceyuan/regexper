d90d3d34ea83518c0ceba71ad8114afddd229c18
yuan <yuan.yuan486@chinaredstar.com>
2017-09-11 17:09:31 +0800

    reg

--- test/test.js
+++ test/test.js
@@ -3,7 +3,11 @@ const assert = require('assert')
 describe('regex', function () {
     it('反向捕获',function(){
         const f = require('../lib/capture')
-        assert.equal(f('2017-09-08'), '08/09/2017', '2017-09-08')
+        assert.equal(f('2017-09-08'), '09/08/2017', '2017-09-08')
+    })
+    it('前瞻',function () {
+        const f = require('../lib/prospect')
+        assert.equal(f('a2*3'),'X2*3','a2*3')
     })
 })
 
